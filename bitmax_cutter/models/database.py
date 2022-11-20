7f22d1eb6c2257a6415a1b03e7708b9c8e89bab0
Mahdi Asadzadeh <mahdi.asadzadeh.programing@gmail.com>
2022-11-20 17:28:52 +0330

    Write declarative base connection.

--- bitmax_cutter/models/database.py
+++ bitmax_cutter/models/database.py
@@ -7,6 +7,7 @@ from bitmax_cutter.core.config import settings
 logger = logging.getLogger('uvicorn.error')
 engine = create_engine(settings.sqlalchemy_database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 try:
     logger.info(f'Init Database from address: {settings.sqlalchemy_database_url.host}')
 except AttributeError:
